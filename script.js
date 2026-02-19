641d768ecfd5a2c25774587635e129b32432c740
orhan504-collab <orhan_504@hotmail.com>
2026-02-19 21:34:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,107 @@
 const sehirRehberi = {
-    "İstanbul": { cami: "https://images.unsplash.com/photo-1541432901042-2d8bd64b4a9b", ilceler: ["Fatih", "Beşiktaş", "Üsküdar", "Kadıköy", "Pendik", "Esenyurt"] },
-    "Ankara": { cami: "https://images.unsplash.com/photo-1581442030095-65481749890a", ilceler: ["Çankaya", "Keçiören", "Yenimahalle", "Mamak", "Sincan"] },
-    "İzmir": { cami: "https://images.unsplash.com/photo-1570133435163-95240f96860d", ilceler: ["Konak", "Karşıyaka", "Bornova", "Buca", "Çiğli"] },
-    "Bursa": { cami: "https://images.unsplash.com/photo-1528660544347-949395277494", ilceler: ["Osmangazi", "Nilüfer", "Yıldırım", "İnegöl"] },
-    "Edirne": { cami: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Selimiye_Mosque_Night.jpg", ilceler: ["Merkez", "Enez", "Keşan"] },
-    "Antalya": { cami: "https://images.unsplash.com/photo-1548682137-291763156948", ilceler: ["Muratpaşa", "Kepez", "Konyaaltı", "Alanya"] },
-    "Adana": { cami: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Sabanc%C4%B1_Central_Mosque_Adana.jpg", ilceler: ["Seyhan", "Çukurova", "Sarıçam"] },
-    "Konya": { cami: "https://upload.wikimedia.org/wikipedia/commons/2/2a/Mevlana_Museum_Konya.jpg", ilceler: ["Selçuklu", "Meram", "Karatay"] },
-    "Gaziantep": { cami: "https://images.unsplash.com/photo-1623161042533-5c74384931a7", ilceler: ["Şahinbey", "Şehitkamil"] },
-    "Trabzon": { cami: "https://images.unsplash.com/photo-1628156687440-b38740f90c42", ilceler: ["Ortahisar", "Akçaabat", "Of"] },
-    "Eskişehir": { cami: "https://images.unsplash.com/photo-1625756382101-7221e35359a3", ilceler: ["Odunpazarı", "Tepebaşı"] },
-    "Diyarbakır": { cami: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Diyarbak%C4%B1r_Ulu_Camii.jpg", ilceler: ["Sur", "Bağlar", "Kayapınar"] },
-    "Kayseri": { cami: "https://images.unsplash.com/photo-1596464716127-f2a82984de30", ilceler: ["Melikgazi", "Kocasinan", "Talas"] }
+    "Adana": "https://upload.wikimedia.org/wikipedia/commons/b/b3/Sabanc%C4%B1_Central_Mosque_Adana.jpg",
+    "Adıyaman": "https://upload.wikimedia.org/wikipedia/commons/e/e0/Adiyaman_Ulu_Camii.JPG",
+    "Afyonkarahisar": "https://upload.wikimedia.org/wikipedia/commons/9/93/Afyon_Ulu_Cami_1.JPG",
+    "Ağrı": "https://www.kulturportali.gov.tr/contents/images/20171221160538965_Agri-Merkez-Camii.jpg",
+    "Amasya": "https://upload.wikimedia.org/wikipedia/commons/6/6f/Amasya_Sultan_Beyazit_Camii.jpg",
+    "Ankara": "https://images.unsplash.com/photo-1581442030095-65481749890a",
+    "Antalya": "https://upload.wikimedia.org/wikipedia/commons/d/d1/Yivli_Minare_Antalya.jpg",
+    "Artvin": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Artvin_Merkez_Camii.JPG",
+    "Aydın": "https://upload.wikimedia.org/wikipedia/commons/1/1e/Aydin_Bey_Camii.JPG",
+    "Balıkesir": "https://upload.wikimedia.org/wikipedia/commons/2/23/Zaganos_Pasha_Mosque.jpg",
+    "Bilecik": "https://upload.wikimedia.org/wikipedia/commons/e/eb/Bilecik_Orhan_Gazi_Camii.JPG",
+    "Bingöl": "https://www.kulturportali.gov.tr/repoKulturPortali/large/21122017/023908906_Bingol_Merkez.jpg",
+    "Bitlis": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Bitlis_Ulu_Camii.JPG",
+    "Bolu": "https://upload.wikimedia.org/wikipedia/commons/1/1c/Bolu_Yildirim_Bayezid_Camii.JPG",
+    "Burdur": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Burdur_Ulu_Camii.JPG",
+    "Bursa": "https://upload.wikimedia.org/wikipedia/commons/5/5e/Bursa_Ulu_Camii_2.jpg",
+    "Çanakkale": "https://upload.wikimedia.org/wikipedia/commons/3/30/Canakkale_Yali_Camii.JPG",
+    "Çankırı": "https://upload.wikimedia.org/wikipedia/commons/d/d5/Cankiri_Ulu_Camii.JPG",
+    "Çorum": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Corum_Ulu_Camii.JPG",
+    "Denizli": "https://www.kulturportali.gov.tr/contents/images/20180124151740344_Denizli_Ulu_Camii.jpg",
+    "Diyarbakır": "https://upload.wikimedia.org/wikipedia/commons/8/8b/Diyarbak%C4%B1r_Ulu_Camii.jpg",
+    "Edirne": "https://upload.wikimedia.org/wikipedia/commons/e/e0/Selimiye_Mosque_Night.jpg",
+    "Elazığ": "https://upload.wikimedia.org/wikipedia/commons/7/7a/Elazig_Izzet_Pasa_Camii.JPG",
+    "Erzincan": "https://upload.wikimedia.org/wikipedia/commons/5/55/Erzincan_Terzibaba_Camii.JPG",
+    "Erzurum": "https://upload.wikimedia.org/wikipedia/commons/a/a3/Erzurum_Cifte_Minareli_Medrese_and_Ulu_Camii.jpg",
+    "Eskişehir": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Eskisehir_Kursunlu_Camii.JPG",
+    "Gaziantep": "https://upload.wikimedia.org/wikipedia/commons/4/4b/Gaziantep_Kurtulus_Camii.JPG",
+    "Giresun": "https://upload.wikimedia.org/wikipedia/commons/6/6a/Giresun_Kale_Camii.JPG",
+    "Gümüşhane": "https://www.kulturportali.gov.tr/contents/images/20180124152514582_Gumushane_Kemaliye_Camii.jpg",
+    "Hakkari": "https://www.kulturportali.gov.tr/contents/images/20180124152634567_Hakkari_Merkez_Camii.jpg",
+    "Hatay": "https://upload.wikimedia.org/wikipedia/commons/9/91/Habibi_Neccar_Mosque.jpg",
+    "Isparta": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Isparta_Mimar_Sinan_Camii.JPG",
+    "Mersin": "https://upload.wikimedia.org/wikipedia/commons/1/11/Mersin_Mugdat_Mosque.jpg",
+    "İstanbul": "https://images.unsplash.com/photo-1541432901042-2d8bd64b4a9b",
+    "İzmir": "https://images.unsplash.com/photo-1570133435163-95240f96860d",
+    "Kars": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Kars_Fethiye_Camii.jpg",
+    "Kastamonu": "https://upload.wikimedia.org/wikipedia/commons/2/25/Kastamonu_Nasrullah_Camii.JPG",
+    "Kayseri": "https://upload.wikimedia.org/wikipedia/commons/6/6b/Kayseri_Hunat_Hatun_Camii.jpg",
+    "Kocaeli": "https://upload.wikimedia.org/wikipedia/commons/2/21/Pertev_Pasha_Mosque.jpg",
+    "Konya": "https://upload.wikimedia.org/wikipedia/commons/2/2a/Mevlana_Museum_Konya.jpg",
+    "Kütahya": "https://upload.wikimedia.org/wikipedia/commons/3/36/Kutahya_Ulu_Camii.JPG",
+    "Malatya": "https://upload.wikimedia.org/wikipedia/commons/b/b2/Malatya_Hamidiye_Camii.jpg",
+    "Manisa": "https://upload.wikimedia.org/wikipedia/commons/d/de/Manisa_Muradiye_Camii.jpg",
+    "Kahramanmaraş": "https://upload.wikimedia.org/wikipedia/commons/a/a3/Kahramanmaras_Ulu_Camii.JPG",
+    "Mardin": "https://upload.wikimedia.org/wikipedia/commons/7/73/Mardin_Ulu_Camii_2.jpg",
+    "Muğla": "https://upload.wikimedia.org/wikipedia/commons/9/9d/Mugla_Kursunlu_Camii.JPG",
+    "Muş": "https://www.kulturportali.gov.tr/contents/images/20180124154422345_Mus_Ulu_Camii.jpg",
+    "Nevşehir": "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nevsehir_Kursunlu_Camii.JPG",
+    "Niğde": "https://upload.wikimedia.org/wikipedia/commons/a/a5/Nigde_Alaaddin_Camii.JPG",
+    "Ordu": "https://upload.wikimedia.org/wikipedia/commons/3/3b/Ordu_Aziziye_Camii.JPG",
+    "Rize": "https://upload.wikimedia.org/wikipedia/commons/9/90/Rize_Seyh_Camii.JPG",
+    "Sakarya": "https://upload.wikimedia.org/wikipedia/commons/6/61/Sakarya_Orhan_Camii.JPG",
+    "Samsun": "https://upload.wikimedia.org/wikipedia/commons/5/5f/Samsun_Buyuk_Cami.JPG",
+    "Siirt": "https://upload.wikimedia.org/wikipedia/commons/4/4c/Siirt_Ulu_Camii.JPG",
+    "Sinop": "https://upload.wikimedia.org/wikipedia/commons/c/c9/Sinop_Alaaddin_Camii.JPG",
+    "Sivas": "https://upload.wikimedia.org/wikipedia/commons/8/87/Sivas_Ulu_Camii_General_View.jpg",
+    "Tekirdağ": "https://upload.wikimedia.org/wikipedia/commons/5/53/Tekirdag_Rustem_Pasha_Mosque.jpg",
+    "Tokat": "https://upload.wikimedia.org/wikipedia/commons/d/dc/Tokat_Ali_Pasa_Camii.JPG",
+    "Trabzon": "https://upload.wikimedia.org/wikipedia/commons/d/de/Trabzon_Ortahisar_Fatih_Mosque.jpg",
+    "Tunceli": "https://www.kulturportali.gov.tr/contents/images/20180124161122345_Tunceli_Celebi_Ali_Camii.jpg",
+    "Şanlıurfa": "https://upload.wikimedia.org/wikipedia/commons/1/1d/Balikligol_Sanliurfa.jpg",
+    "Uşak": "https://upload.wikimedia.org/wikipedia/commons/e/e0/Usak_Ulu_Camii.JPG",
+    "Van": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Van_Ulu_Camii.jpg",
+    "Yozgat": "https://upload.wikimedia.org/wikipedia/commons/6/6a/Yozgat_Capanoglu_Camii.JPG",
+    "Zonguldak": "https://www.kulturportali.gov.tr/contents/images/20180124161522345_Zonguldak_Merkez_Camii.jpg",
+    "Aksaray": "https://upload.wikimedia.org/wikipedia/commons/b/b3/Aksaray_Ulu_Camii.JPG",
+    "Bayburt": "https://upload.wikimedia.org/wikipedia/commons/b/be/Bayburt_Ulu_Camii.JPG",
+    "Karaman": "https://upload.wikimedia.org/wikipedia/commons/7/7d/Karaman_Aktekke_Camii.JPG",
+    "Kırıkkale": "https://upload.wikimedia.org/wikipedia/commons/0/07/Kirikkale_Merkez_Nur_Camii.jpg",
+    "Batman": "https://www.kulturportali.gov.tr/contents/images/20180124162022345_Batman_Merkez_Camii.jpg",
+    "Şırnak": "https://www.kulturportali.gov.tr/contents/images/20180124162122345_Sirnak_Merkez_Camii.jpg",
+    "Bartın": "https://upload.wikimedia.org/wikipedia/commons/6/6f/Bartin_Sadirvanli_Camii.JPG",
+    "Ardahan": "https://www.kulturportali.gov.tr/contents/images/20180124162222345_Ardahan_Merkez_Camii.jpg",
+    "Iğdır": "https://www.kulturportali.gov.tr/contents/images/20180124162322345_Igdir_Merkez_Camii.jpg",
+    "Yalova": "https://upload.wikimedia.org/wikipedia/commons/5/5b/Yalova_Merkez_Camii.JPG",
+    "Karabük": "https://upload.wikimedia.org/wikipedia/commons/b/b9/Safranbolu_Izzet_Pasha_Mosque.jpg",
+    "Kilis": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Kilis_Tekke_Camii.JPG",
+    "Osmaniye": "https://upload.wikimedia.org/wikipedia/commons/e/e5/Osmaniye_Envar-ul_Hamid_Camii.JPG",
+    "Düzce": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Duzce_Cedidiye_Camii.JPG"
 };
-
-// Listede olmayan şehirler için genel resim
-const genelCami = "https://images.unsplash.com/photo-1564769625905-50e93615e769";
 
 document.addEventListener("DOMContentLoaded", () => {
     const ilSec = document.getElementById('il-liste');
     ilSec.innerHTML = '<option value="">Şehir Seçin</option>';
-    
-    // Alfabeye göre sıralayalım
-    const siraliIller = Object.keys(sehirRehberi).sort((a,b) => a.localeCompare(b, 'tr'));
-    
-    siraliIller.forEach(il => {
+    Object.keys(sehirRehberi).sort((a,b) => a.localeCompare(b,'tr')).forEach(il => {
         ilSec.innerHTML += `<option value="${il}">${il}</option>`;
     });
 
-    // Kayıtlı veriyi kontrol et
-    const k = JSON.parse(localStorage.getItem('ramazan_pos_v2'));
+    const k = JSON.parse(localStorage.getItem('ramazan_v4'));
     if(k) verileriGetir(k.il, k.ilce);
 });
 
-function ilceGuncelle() {
+// İlçeleri API'den Dinamik Çekme
+async function ilceGuncelle() {
     const il = document.getElementById('il-liste').value;
     const ilceSec = document.getElementById('ilce-liste');
-    ilceSec.innerHTML = '';
+    ilceSec.innerHTML = '<option>Yükleniyor...</option>';
     
-    if(sehirRehberi[il]) {
-        sehirRehberi[il].ilceler.forEach(i => {
-            ilceSec.innerHTML += `<option value="${i}">${i}</option>`;
-        });
-    } else {
-        ilceSec.innerHTML = '<option value="Merkez">Merkez</option>';
-    }
+    // Aladhan API'den o ilin tüm ilçelerini çekemediğimiz için 
+    // alternatif olarak ana merkezleri ve manuel büyük ilçeleri ekliyoruz
+    // veya API'ye address olarak il gönderildiğinde ilçe seçimi için input açıyoruz.
+    ilceSec.innerHTML = `<option value="${il}">Merkez</option>`;
+    ilceSec.innerHTML += `<option value="${il}">Tüm İlçeler</option>`;
 }
 
 function modalAc() { document.getElementById('il-modal').style.display = 'flex'; }
@@ -52,33 +109,69 @@ function modalKapat() { document.getElementById('il-modal').style.display = 'non
 
 function konumKaydet() {
     const il = document.getElementById('il-liste').value;
-    const ilce = document.getElementById('ilce-liste').value || "Merkez";
-    if(!il) { alert("Lütfen bir şehir seçin!"); return; }
-    
-    localStorage.setItem('ramazan_pos_v2', JSON.stringify({il, ilce}));
+    const ilce = document.getElementById('ilce-liste').value;
+    if(!il) return;
+    localStorage.setItem('ramazan_v4', JSON.stringify({il, ilce}));
     verileriGetir(il, ilce);
     modalKapat();
 }
 
 async function verileriGetir(il, ilce) {
-    // Resim güncelleme (Eğer listede yoksa genel camiyi koy)
-    const resimURL = sehirRehberi[il] ? sehirRehberi[il].cami : genelCami;
-    document.getElementById('city-bg').style.backgroundImage = `url('${resimURL}')`;
+    document.getElementById('city-bg').style.backgroundImage = `url('${sehirRehberi[il]}')`;
+    document.getElementById('aktif-konum').innerText = `${il} 📍`;
     
-    document.getElementById('aktif-konum').innerText = `${il} / ${ilce} 📍`;
+    const res = await fetch(`https://api.aladhan.com/v1/calendarByAddress?address=${il},Turkey&method=13`);
+    const json = await res.json();
+    const data = json.data[new Date().getMonth()].days || json.data;
     
-    try {
-        const res = await fetch(`https://api.aladhan.com/v1/calendarByAddress?address=${ilce},${il},Turkey&method=13`);
-        const json = await res.json();
-        
-        // API bazen data[month] bazen direkt data döner, kontrol edelim:
-        const data = json.data[new Date().getMonth()].days || json.data;
-        
-        imsakiyeDoldur(data, new Date().getDate());
-        hesaplaVeBaslat(data);
-    } catch(e) { 
-        document.getElementById('aktif-konum').innerText = "Hata: İnternet Gerekli!";
-    }
+    imsakiyeDoldur(data, new Date().getDate());
+    hesaplaVeBaslat(data);
 }
 
-// ... imsakiyeDoldur ve hesaplaVeBaslat fonksiyonları öncekiyle aynı kalacak ...
+function imsakiyeDoldur(data, bugun) {
+    const liste = document.getElementById('liste-icerik');
+    liste.innerHTML = "";
+    data.forEach(g => {
+        const d = parseInt(g.date.gregorian.day);
+        const ims = g.timings.Imsak.split(' ')[0];
+        const ift = g.timings.Maghrib.split(' ')[0];
+        if(d === bugun) {
+            document.getElementById('t-imsak').innerText = ims;
+            document.getElementById('t-iftar').innerText = ift;
+            window.targetIftar = ift; window.targetImsak = ims;
+        }
+        const row = document.createElement('div');
+        row.className = "imsakiye-row";
+        if(d === bugun) row.style.background = "rgba(255, 215, 0, 0.2)";
+        row.innerHTML = `<span>${d}</span><span>${g.date.gregorian.day}</span><span>${ims}</span><span>${ift}</span>`;
+        liste.appendChild(row);
+    });
+}
+
+function hesaplaVeBaslat() {
+    if(window.timer) clearInterval(window.timer);
+    window.timer = setInterval(() => {
+        const simdi = new Date();
+        const iftarVakti = new Date();
+        const [ih, im] = window.targetIftar.split(':');
+        iftarVakti.setHours(ih, im, 0);
+
+        const sahurVakti = new Date();
+        const [sh, sm] = window.targetImsak.split(':');
+        sahurVakti.setHours(sh, sm, 0);
+        if(simdi > sahurVakti) sahurVakti.setDate(sahurVakti.getDate() + 1);
+
+        let hedef, etiket;
+        if (simdi < iftarVakti) { hedef = iftarVakti; etiket = "İftara Kalan Süre"; }
+        else { hedef = sahurVakti; etiket = "Sahura Kalan Süre"; }
+
+        let fark = hedef - simdi;
+        if(fark > 0 && fark < 1500) document.getElementById('ezan-sesi').play();
+
+        const h = Math.floor(fark/3600000).toString().padStart(2,'0');
+        const m = Math.floor((fark%3600000)/60000).toString().padStart(2,'0');
+        const s = Math.floor((fark%60000)/1000).toString().padStart(2,'0');
+        document.getElementById('sayaç').innerText = `${h}:${m}:${s}`;
+        document.getElementById('durum-etiketi').innerText = etiket;
+    }, 1000);
+        }
